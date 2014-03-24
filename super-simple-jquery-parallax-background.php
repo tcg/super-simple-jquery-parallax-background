605e1166c24ec2ad24ceafba30724575f700404f
Tommy George <github.com@tommygeorge.com>
2014-03-24 10:07:04 -0500

    Remove stray comment.

--- super-simple-jquery-parallax-background.php
+++ super-simple-jquery-parallax-background.php
@@ -15,7 +15,6 @@ function super_simple_jquery_parallax_background_action() {
     // Register and queue Javascript file.
     wp_register_script(
         'super_simple_jquery_parallax_background_js',
-        // dirname(__FILE__)
         plugins_url('/assets/js/super-simple-jquery-parallax-background.js', __FILE__),
         array('jquery'), // Dependency.
         false, // Specific version, if exists.
